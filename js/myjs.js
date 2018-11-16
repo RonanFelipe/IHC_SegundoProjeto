d17b650cfeb5805ec892ef1f8b59fa9388ca70d7
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2018-11-16 01:46:55 -0200

    Second card and first modal are ready.

--- js/myjs.js
+++ js/myjs.js
@@ -21,8 +21,21 @@ function getTempoLeitura() {
     for (let i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked){
             user.tempoLeituraDiaria = radios[i].value;
-            break;
+            let modal = document.getElementById('myModal');
+            let span = document.getElementsByClassName("close")[0];
+            modal.style.display = "block";
+
+            span.onclick = function () {
+                modal.style.display = "none";
+            };
+
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
         }
     }
 }
+
 
