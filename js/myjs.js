534ff9dbc98be266c7bcf68ed6a7c2048328f6a8
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2018-11-16 20:30:59 -0200

    Fourth card and modals done.

--- js/myjs.js
+++ js/myjs.js
@@ -45,10 +45,46 @@ function getTempoLeitura() {
 function getAge() {
     let radios = document.getElementsByName('age');
     for (let i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            user.idade = radios[i].value;
+        if (radios[0].checked) {
+            user.idade = radios[0].value;
             let modal = document.getElementById('myModalAge');
             let span = document.getElementById('closeModal');
+            let titulo = document.getElementById('titulo').innerHTML = "Se você tem até 24 anos.";
+            let texto = document.getElementById('textoModal').innerHTML = "A recomendação é de 47 minutos de leitura contínua por dia!";
+            modal.style.display = "block";
+
+            span.onclick = function () {
+                modal.style.display = "none";
+            };
+
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            };
+        } else if (radios[1].checked) {
+            user.idade = radios[1].value;
+            let modal = document.getElementById('myModalAge');
+            let span = document.getElementById('closeModal');
+            let titulo = document.getElementById('titulo').innerHTML = "Se você tem de 25 a 59 anos.";
+            let texto = document.getElementById('textoModal').innerHTML = "A recomendação é de 37 minutos de leitura contínua por dia!";
+            modal.style.display = "block";
+
+            span.onclick = function () {
+                modal.style.display = "none";
+            };
+
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            };
+        } else if (radios[2].checked) {
+            user.idade = radios[2].value;
+            let modal = document.getElementById('myModalAge');
+            let span = document.getElementById('closeModal');
+            let titulo = document.getElementById('titulo').innerHTML = "Se você tem mais de 60 anos.";
+            let texto = document.getElementById('textoModal').innerHTML = "A recomendação é de 17 minutos de leitura contínua por dia!";
             modal.style.display = "block";
 
             span.onclick = function () {
