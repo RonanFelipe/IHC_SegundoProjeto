07e7abcd4c42fa6d8c2ec6c155848ea469c82ca5
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2018-11-16 20:14:53 -0200

    Third modal Ok!

--- js/myjs.js
+++ js/myjs.js
@@ -27,13 +27,39 @@ function getTempoLeitura() {
 
             span.onclick = function () {
                 modal.style.display = "none";
+                document.getElementById('leituraTempo').style.display = "none";
+                document.getElementById('idadeUsuario').style.display = "block";
+            };
+
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                    document.getElementById('leituraTempo').style.display = "none";
+                    document.getElementById('idadeUsuario').style.display = "block";
+                }
+            }
+        }
+    }
+}
+
+function getAge() {
+    let radios = document.getElementsByName('age');
+    for (let i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            user.idade = radios[i].value;
+            let modal = document.getElementById('myModalAge');
+            let span = document.getElementById('closeModal');
+            modal.style.display = "block";
+
+            span.onclick = function () {
+                modal.style.display = "none";
             };
 
             window.onclick = function (event) {
                 if (event.target == modal) {
                     modal.style.display = "none";
                 }
-            }
+            };
         }
     }
 }
